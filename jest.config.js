15785c8c3dfd076fd3e4c6282d86cf5d3d6479aa
87ehnosso <114113142+danilolopes87@users.noreply.github.com>
2024-11-06 18:13:36 -0300

    Storybooks inatlado e funcionando

--- jest.config.js
+++ jest.config.js
@@ -9,10 +9,25 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
+  collectCoverage: true,
   collectCoverageFrom: [
-    '!src/app/**', // should be tested in e2e
-    '!src/lib/registry.tsx'
-  ]
+  'src/**/*.{js,jsx,ts,tsx}',   // inclui apenas arquivos do código-fonte
+  '!src/app/**',                // exclui o diretório app (usado em testes e2e)
+  '!src/lib/registry.tsx',      // exclui arquivos específicos irrelevantes
+  '!src/types/**',              // exclui tipos (TS)
+  '!src/styles/**',             // exclui arquivos de estilo
+  '!src/.next/**',              // exclui arquivos do build do Next.js
+  '!src/**/index.ts',           // exclui arquivos index.ts
+  '!**/*.d.ts',                 // exclui arquivos de tipos do TypeScript
+  '!**/node_modules/**'         // exclui node_modules
+  ],
+  // https://github.com/styled-components/styled-components/issues/4081
+  // v6 of styled-components doesn't inject styles in test environment
+  // we should force it to use the browser version
+  moduleNameMapper: {
+    '^styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js'
+  }
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
